9ec53b77cca6e0510fc6f8bfad82e1106b4b89b5
caisoc200 <caiosc200@yahoo.com.br>
2021-09-23 00:34:04 -0300

    settings json

---
+++ src/repository/index.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://foodspot:ppKoCn4ZdpLOT3S1hNUyJsmexQ2FAv8a33lMQ0nWqP2CKDNjPfNWP5NegRyswm2sZeN0Cwe5eHcmVPsvBUYqAw==@foodspot.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@foodspot@")
+
+module.exports = {
+    restaurantemodel: require('./models/restaurante')
+}
