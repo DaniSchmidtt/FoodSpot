ce1ab9bf188a60edda717baa1d60267176cd59ca
Giovannapls <37786977+Giovannapls@users.noreply.github.com>
2021-10-28 20:57:12 -0300

    [fix] cadastro e login ts

--- food-spot-app/src/app/cadastro/cadastro.page.ts
+++ food-spot-app/src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,9 @@
+import { AppComponent } from '../app.component';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute ,Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +11,70 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
+  public api: Observable<any>;
   public cadastro: string;
-  constructor(private activatedRoute: ActivatedRoute) { }
+  public nome: string;
+  public email: string;
+  public password: string;
+  public secpassword: string;
+  public retorno;
+  router: Router;
+  constructor(private activatedRoute: ActivatedRoute, public httpClient: HttpClient,
+    public appComponent: AppComponent, public alertCtrl: AlertController, router: Router) { 
+      this.router = router
+    }
 
+    Cadastrar()
+    {
+      if(false){
+        this.showAlert("Falha no Cadastro", "Esta faltando infomações!");
+      }else{
+        if(this.password === this.secpassword){
+          let postData = {
+            "login": this.email,
+            "senha": this.password
+          }  
+          this.httpClient.post("http://localhost:1337/login", postData)
+            .subscribe(data => {
+              this.Auth()      
+            }, error => {
+              this.showAlert("Falha no Cadastro", "Cadastro ja existe!");
+            });
+        }else{
+          this.showAlert("Falha no Cadastro", "Senhas diferentes!");
+        }
+
+      }
+
+    }
+
+    Auth() {
+      let postData = {
+        "login": this.email,
+        "senha": this.password
+      }  
+      this.httpClient.post("http://localhost:1337/login/auth", postData)
+        .subscribe(data => {
+          this.retorno = data;
+          this.appComponent.isloged = this.retorno.auth;
+          this.appComponent.user = this.email;
+          this.showAlert("Sucesso", "Login realizado");  
+          this.router.navigateByUrl('restaurantes/Restaurantes')      
+        }, error => {
+          this.showAlert("Login failed", "Login ou senha não estão cadastrados");
+        });
+
+    }
+    async showAlert(cabecalho, menssagem) {
+      const alert = await this.alertCtrl.create({
+        header: cabecalho,
+        subHeader: '',
+        message: menssagem,
+        buttons: ['OK']
+      });
+      await alert.present();
+      const result = await alert.onDidDismiss();
+    }
   ngOnInit() {
     this.cadastro = this.activatedRoute.snapshot.paramMap.get('id');
   }
